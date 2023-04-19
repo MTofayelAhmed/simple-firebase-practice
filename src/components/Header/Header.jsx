f64fdea77cd7c592cd85e5c14f3c2f9c1fc566d5
MTofayelAhmed <113205143+MTofayelAhmed@users.noreply.github.com>
2023-04-19 12:30:14 +0600

    header and login task done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Header.css'
 
 const Header = () => {
   return (
-    <div>
-      
-    </div>
+    <nav className='header' >
+        <Link to='/'>Home</Link>
+        <Link to='/login'>Login</Link>
+        <Link to='/register'>Register</Link>
+    </nav>
   );
 };
 
